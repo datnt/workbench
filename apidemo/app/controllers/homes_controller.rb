352fb3d15a326c279ffe63ae707e122f2dcc18a7
datnt <khodatabase@gmail.com>
2013-03-30 14:35:24 +0700

    success with authentication by id/pass from client

--- apidemo/app/controllers/homes_controller.rb
+++ apidemo/app/controllers/homes_controller.rb
@@ -7,7 +7,16 @@ class HomesController < ApplicationController
     access_token = client1.password.get_token('ngothiendat@gmail.com', '111111')
     puts access_token.token
   end
-  def show
-    render :text => "hello SHOW ACTION"
+  def login
+    
+  end
+  def authentication
+    app_id="8b08fd049e27b76e3fef76a618ed5e21b1a58aade53b9c6ccfaee3a2f6126f62"
+    secret="510ac64ceaf449cd7c7f5ced7233a5655d4d7c1ff6873b3f39c52ab45cab27d6"
+
+    client1=OAuth2::Client.new(app_id,secret,site:"http://localhost:3000/")
+    access_token = client1.password.get_token(params[:email], params[:password])
+
+    render :text => access_token.token
   end
 end
--- apidemo/config/routes.rb
+++ apidemo/config/routes.rb
@@ -55,6 +55,11 @@ Apidemo::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  resources :homes
-  match 'todo/callback' => 'homes#show'
+  resources :homes do
+    collection do
+      get :login
+      post :authentication
+    end
+  end
+
 end
