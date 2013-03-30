3ac7466e3868d6d2fd2556aa0619a5da19abeeec
datnt <khodatabase@gmail.com>
2013-03-30 14:57:42 +0700

    handle the case of login failed from client

--- apidemo/app/controllers/homes_controller.rb
+++ apidemo/app/controllers/homes_controller.rb
@@ -11,12 +11,17 @@ class HomesController < ApplicationController
     
   end
   def authentication
-    app_id="8b08fd049e27b76e3fef76a618ed5e21b1a58aade53b9c6ccfaee3a2f6126f62"
-    secret="510ac64ceaf449cd7c7f5ced7233a5655d4d7c1ff6873b3f39c52ab45cab27d6"
+    begin
+      app_id="8b08fd049e27b76e3fef76a618ed5e21b1a58aade53b9c6ccfaee3a2f6126f62"
+      secret="510ac64ceaf449cd7c7f5ced7233a5655d4d7c1ff6873b3f39c52ab45cab27d6"
 
-    client1=OAuth2::Client.new(app_id,secret,site:"http://localhost:3000/")
-    access_token = client1.password.get_token(params[:email], params[:password])
+      client1=OAuth2::Client.new(app_id,secret,site:"http://localhost:3000/")
+      access_token = client1.password.get_token(params[:email], params[:password])
 
-    render :text => access_token.token
+      render :text => access_token.token
+    rescue
+      render :text => 'login failed'
+    end
+
   end
 end
