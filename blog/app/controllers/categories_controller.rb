20dc92746106f6d3283203c06b4883e902743e49
datnt <khodatabase@gmail.com>
2013-03-31 11:03:17 +0700

    prepare form new cate

--- blog/app/controllers/categories_controller.rb
+++ blog/app/controllers/categories_controller.rb
@@ -4,4 +4,10 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+  def new
+    @category = Category.new
+  end
+  def create
+    
+  end
 end
