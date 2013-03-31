2ea97410a900ca4e1d572c1cecbd02d749bdf219
datnt <khodatabase@gmail.com>
2013-03-31 14:51:38 +0700

    edit with selected category checked

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :article_scopes
   belongs_to :user
 
   acts_as_commentable
 
+  def categories
+    Category.where("id in (?)",self.article_scopes.map(&:category_id))
+  end
   
 end
--- blog/app/models/article_scope.rb
+++ blog/app/models/article_scope.rb
@@ -1,5 +1,4 @@
 class ArticleScope < ActiveRecord::Base
   belongs_to :article
-  has_many :category
 
 end
