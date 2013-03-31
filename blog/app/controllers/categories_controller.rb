758423958d99746c6498a1e1c4198a27ac2d6f5f
datnt <khodatabase@gmail.com>
2013-03-31 11:01:38 +0700

    update data with category

--- blog/app/controllers/categories_controller.rb
+++ blog/app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   before_filter :authorize, only: [:edit, :update]
   
   def index
-    @articles = Article.all
+    @categories = Category.all
   end
 end
