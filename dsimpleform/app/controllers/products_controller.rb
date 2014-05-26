3cffae73c9f4cd6405b390d0af49988ffd781ad4
datntDesktop <datefield@yahoo.com>
2014-05-26 16:50:06 +0700

    apply validation to show simple form error display ability

--- dsimpleform/app/controllers/products_controller.rb
+++ dsimpleform/app/controllers/products_controller.rb
@@ -5,4 +5,13 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  def create
+    product_params = params.require(:product).permit(:sub_category_id, :name, :description)
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to :action => "index"
+    else
+      render :action => "new"
+    end
+  end
 end
--- dsimpleform/app/models/product.rb
+++ dsimpleform/app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :sub_category
+  
+  validates_presence_of :sub_category_id
+  validates_presence_of :name
+  validates_presence_of :description
 end
