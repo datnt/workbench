1e2614ebae1c045cb2c39870099a875c7a687c8f
datntDesktop <datefield@yahoo.com>
2014-06-12 16:10:35 +0700

    setup project dsimple form to have ability to run cucumber as Rack environment, but still having problem with javascript, may be because of rvm error

--- dsimpleform/features/step_defs/steps.rb
+++ dsimpleform/features/step_defs/steps.rb
@@ -1,12 +1,27 @@
-Given(/^I am on the YouTube home page$/) do
-  visit 'http://www.youtube.com'
-end
+#Given(/^I am on the YouTube home page$/) do
+#  visit 'http://www.youtube.com'
+#end
+#
+#When(/^I search for "(.*?)"$/) do |search_term|
+#  fill_in 'search_query', :with => search_term
+#  click_on 'search-btn'
+#end
+#
+#Then(/^videos of Animal Planet are returned$/) do
+#  page.should have_content 'Animal Planet'
+#end
+World(Rack::Test::Methods)
 
-When(/^I search for "(.*?)"$/) do |search_term|
-  fill_in 'search_query', :with => search_term
-  click_on 'search-btn'
-end
 
-Then(/^videos of Animal Planet are returned$/) do
-  page.should have_content 'Animal Planet'
+Given(/^I open new product site$/) do
+  visit('/products/new')
+end
+When(/^I input new product$/) do
+  fill_in 'Name', :with => 'Matt'
+  fill_in 'Description', :with => 'Catch 22'
+  select 'Meat', :from => 'Sub category'
+  click_on 'Create Product'
+end
+Then(/^I should see the product created confirmed$/) do
+  page.should have_text 'hello world'
 end
--- dsimpleform/features/support/env.rb
+++ dsimpleform/features/support/env.rb
@@ -1,2 +1,20 @@
 require 'capybara/cucumber'
-Capybara.default_driver = :selenium
+require 'rspec/expectations'
+require 'cucumber/rails'
+
+Capybara.javascript_driver = :selenium
+
+#Capybara.default_driver = :selenium
+#
+#Capybara.default_driver = :rack_test
+#Capybara.register_driver :selenium do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
+
+#
+#Capybara.configure do |config|
+#  config.run_server = false
+#  #config.default_driver = :selenium
+#  config.default_driver = :rack_test
+#  config.app_host = 'http://localhost:3000' # change url
+#end
